4d90ff6d5ec8a79e75665bb024ef6aa213dcbebd
nikraj <35949589+nikraj@users.noreply.github.com>
2018-02-07 22:11:36 +0530

    Update index.js
    trdfg

--- index.js
+++ index.js
@@ -19,10 +19,8 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function (req, res) {
     var speech =
-  req.body.result &&
-  req.body.result.parameters &&
-  req.body.result.parameters.echoText
-   ? req.body.result.parameters.echoText
+  req.body.result
+   ? req.body.result
  : "Seems like some problem. Speak again.";
     textapi.classify({ 'text': speech }, function (error, response) {
         if (error === null) {
