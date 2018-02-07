cdb11088cebc73439801f03e937f057b9a2f98cf
nikraj <35949589+nikraj@users.noreply.github.com>
2018-02-07 12:18:25 +0530

    Update index.js
    ghj

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function (req, res) {
-    textapi.classify({ 'text': 'Microsoft is planning to grow!!' }, function (error, response) {
+    textapi.classify({ 'text': 'Titanic is awesome to watch!!' }, function (error, response) {
         if (error === null) {
             //var speech = response
             return res.json({
